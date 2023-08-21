4c67455df186d1ac49148541268330d947dc5ee1
BowangLan <blan2@uw.edu>
2023-08-21 12:13:12 -0700

    Updated home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,12 @@ export default async function Home() {
     <DefaultLayout>
       <section className="fcenter">
         <div className="flex flex-col items-center space-y-2 tracking-wider md:space-y-4 md:text-4xl">
-          <div className="text-[2rem] sm:text-[2.5rem] md:text-[3rem] lg:text-[4rem]">
+          <div className="text-[2rem] sm:text-[2.5rem] md:text-[3rem] lg:text-[3.2rem]">
             Hello, {"I'm "}
             <span className="font-bold h1-gradient">Bowang Lan</span>
           </div>
-          <div className="text-center lg:pt-3 sm:text-xl md:text-2xl lg:text-3xl">
-            A full-stack developer, UI designer, and data scientist
+          <div className="text-center lg:pt-3 sm:text-xl md:text-2xl lg:text-3xl text-slate-300">
+            A full-stack developer, data scientest, and UI/UX designer
           </div>
           <ContactMeButton />
           <div className="pt-4">

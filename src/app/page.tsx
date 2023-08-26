cd769a0a82012a48c576933b3e888c62caf00ca5
BowangLan <blan2@uw.edu>
2023-08-26 14:30:06 -0700

    Updated hero font size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default async function Home() {
     <DefaultLayout>
       <section className="h-[var(--h-main)] fcenter">
         <div className="flex flex-col items-center space-y-2 md:space-y-4 md:text-4xl">
-          <h1 className="text-[2rem] text-center tracking-normal sm:text-[2.5rem] md:text-[3rem] lg:text-[3.2rem] sm:tracking-wider">
+          <h1 className="text-[2rem] text-center tracking-normal sm:text-[2.5rem] md:text-[3.25rem] lg:text-[3.5rem] 2xl:text-[4rem] sm:tracking-wider">
             Hello, {"I'm "}
             <span className="font-bold primary-gradient">Bowang Lan</span>
           </h1>

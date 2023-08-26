d799524387b6003e046037259b7f175f97853618
BowangLan <blan2@uw.edu>
2023-08-26 09:44:47 -0700

    Update DefaultLayout.tsx

--- src/components/layout/DefaultLayout.tsx
+++ src/components/layout/DefaultLayout.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function DefaultLayout({ children }: { children: React.ReactNode }) {
   return (
-    <main className='z-10 flex flex-col px-4 md:px-8'>
+    <main className='z-10 flex flex-col items-stretch max-w-full px-4 md:px-8'>
       {children}
     </main>
   )

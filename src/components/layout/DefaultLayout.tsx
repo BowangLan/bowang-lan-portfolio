d2a0f170433a4dcdd04998b2694a5aca85cc456f
BowangLan <blan2@uw.edu>
2023-08-26 10:05:00 -0700

    Updated width for super wide screens

--- src/components/layout/DefaultLayout.tsx
+++ src/components/layout/DefaultLayout.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function DefaultLayout({ children }: { children: React.ReactNode }) {
   return (
-    <main className='z-10 flex flex-col items-stretch max-w-full px-4 md:px-8'>
+    <main className='z-10 flex flex-col items-stretch max-w-full px-4 mx-auto md:px-8 2xl:max-w-[1500px]'>
       {children}
     </main>
   )

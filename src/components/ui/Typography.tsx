ea48456bc5abb61985e3cdc1ac0c54b378068d41
BowangLan <blan2@uw.edu>
2023-08-26 09:44:33 -0700

    Update Typography.tsx

--- src/components/ui/Typography.tsx
+++ src/components/ui/Typography.tsx
@@ -1,5 +1,5 @@
 export function SectionHeader({ children }: { children: React.ReactNode }) {
   return (
-    <h2 className="py-10 text-3xl font-medium md:text-4xl text-blue-50">{children}</h2>
+    <h2 className="py-10 text-3xl font-medium sm:py-12 md:text-4xl text-blue-50">{children}</h2>
   );
 }

bc4e265e7143653b532f5fc4a3278ec042137879
BowangLan <blan2@uw.edu>
2023-08-26 09:44:50 -0700

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -60,7 +60,7 @@ export default function Header() {
 
   return (
     <header
-      className="relative flex items-center justify-between h-[var(--h-header)] px-page trans"
+      className="relative flex items-center justify-between h-[var(--h-header)] px-page trans w-screen"
       style={{
         display: pathname.startsWith("/admin") ? "none" : "flex",
       }}

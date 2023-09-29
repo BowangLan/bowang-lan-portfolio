318f5d28a51f8f0ea5aef53e1b93c5abf2a76290
BowangLan <blan2@uw.edu>
2023-09-29 09:26:59 -0700

    Updated resume

---
+++ src/app/resume/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function ResumeProjectItem({ project }: { project: Project }) {
+  return (
+    <div className='flex'>
+      <h2 className=''>
+        {project.title}
+      </h2>
+    </div>
+  )
+}
+
+export default function ResumeNextPage() {
+  return (
+    <div>
+
+    </div>
+  )
+}

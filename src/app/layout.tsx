a72b7dde1009f007e0fd2174f11ed139468901ae
BowangLan <60339191+BowangLan@users.noreply.github.com>
2023-11-09 10:09:08 -0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,27 @@
 import { Analytics } from "@vercel/analytics/react";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
-import Particles from "@/components/Particles";
+// import Particles from "@/components/Particles";
 import { Raleway } from "next/font/google";
+// import { GeistSans } from "geist/font";
 import "./globals.css";
 import { GlobalWrapper } from "@/components/GlobalWrapper";
+import dynamic from "next/dynamic";
 
-const raleway = Raleway({ display: "swap", subsets: ["latin"] });
+const font = Raleway({ display: "swap", subsets: ["latin"] });
+
+const Particles = dynamic(() => import("@/components/Particles"), {
+  ssr: false,
+});
 
 export const metadata = {
   title: "Bowang Lan",
   description:
     "Bowang Lan is a software engineer based in Seattle, WA and a student at University of Washington.",
+  visualViewport: {
+    width: "device-width",
+    initialScale: 1.0,
+  },
 };
 
 export default function RootLayout({
@@ -22,8 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        // className={"grid h-screen max-w-screen"}
-        className={raleway.className + " grid h-screen max-w-screen"}
+        className={font.className + " grid h-screen max-w-screen font-light"}
         style={{
           gridTemplateRows: "auto 1fr auto",
         }}
